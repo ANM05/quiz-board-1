63e5a7b8b7152afb6c17f1a480e2babec790f83e
Daniel Montet <dmontetproff@gmail.com>
2019-02-23 17:29:04 +0300

    Add exception to catch any undefined value before concantenation

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 //UI LOGIC
-var choices=['question1 : chocolate','question2 : vanilla']
-var questionids=[]
-var answers=[]
-var results=[]
+var choices=['question1:chocolate','question2:vanilla']//STORES CONCACTED FORM OF ANSWER AND QUESTION ID 
+var questionids=[];//CONTAINS ALL PULLED QUESTION IDS
+var answers=[];//CONTAINS ALL INPUT ANSWERS
+var results=[];//CONTAINS CONCATED USER ANSWERS AND QUESTION IDS
+
 
 $(document).ready(function(){
   $("#intronav").click(function(){
@@ -29,24 +30,32 @@ $(document).ready(function(){
   }) 
 
   traverse();
-  alert(answers)
-  compare();
+  concantenate();
   alert(results);
+  compare(results,choices)
+  alert(marks);
   
 
 
 })
 
+//BUSINESS LOGIC
+//get values from pages submitted.
+
+//for every qn id,check the value selected and compare with answer stored in variable array here, else continue after submission
+//pass in qestion id then traverse and check input if checked, if its checked return value
+
+
 function traverse(){
 
   for(item=0;item<questionids.length;item+=1){
-    if(($("li#"+questionids[item]+" input").is(":checked")===false)){//
-      alert("no selections made")
+    if(($("li#"+questionids[item]+" input").is(":checked")===true)){//
+      
+      var answer=($("li#"+questionids[item]+" input:checked").val())
+      answers.push(answer)//ANSWERS GLOBAL VARIABLE
     }
     else{
-      var answer=($("li#"+questionids[item]+" input:checked").val())
-      answers.push(answer)
-      
+      alert("no selections made") 
 
     }
     
@@ -54,15 +63,39 @@ function traverse(){
       }
 
 }
-function compare(){
+
+
+function concantenate(){//FUNCTION TO CONCAT QUESTIONID AND ANSWER
   for(x=0,y=0;x<answers.length,y<questionids.length;x+=1,y+=1){
-    var result= questionids[y] + " : " + answers[x];
-    results.push(result)
+    try{
+      if(answers[x]===undefined) throw "You must select value"
+
+      else{
+        var result= questionids[y] + ":" + answers[x];
+        results.push(result)//RESULTS GLOBAL VARIABLE
+      }
+    }
+    catch(err){
+      alert(err)
+    }
 
     }
 }
 
+marks=0;
+function compare(list1,list2){///FUNCTION TO COMPARE ENTITIES IN TWO DIFFERENT ARRAYS AND COUNT TOTAL MARKS
+  
+  for(x=0;x<list1.length;x+=1){
+    do{
+      
+      marks+=parseInt('1');
 
+    }
+    while(list1[x] in list2)
+    
+    }
+    
+}
 
 
 
@@ -72,8 +105,3 @@ function compare(){
 
 
 
-//BUSINESS LOGIC
-//get values from pages submitted.
-
-//for every qn id,check the value selected and compare with answer stored in variable array here, else continue after submission
-//pass in qestion id then traverse and check input if checked, if its checked return value
